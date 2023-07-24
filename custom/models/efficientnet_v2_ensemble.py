542eaef6b55ba9a2f250d48f85ac5362c7a7d04b
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-24 11:51:47 +0800

    add efficientnet v2 ensemble training configs

---
+++ custom/models/efficientnet_v2_ensemble.py
@@ -0,0 +1,25 @@
+#%%
+import torch
+import torch.nn as nn
+from manafaln.core.builders import ModelBuilder
+from torchvision.models import efficientnet_v2_s, efficientnet_v2_m
+
+#%%
+
+class efficientnetv2_ensemble(nn.Module):
+    def __init__(
+        self, 
+        num_classes: int,
+    ):
+        super().__init__()
+        self.model1 = efficientnet_v2_s(weights='DEFAULT')
+        self.model2 = efficientnet_v2_m(weights='DEFAULT')
+        self.linear = nn.Linear(1000, num_classes)
+
+    def forward(self, x):
+        x1 = self.model1(x)
+        x2 = self.model2(x)
+        x_combined = (x1 + x2)/2
+        x_output = self.linear(x_combined)
+        return x_output
+
