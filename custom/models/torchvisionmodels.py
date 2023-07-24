bb8212f16832d2de71871361efc08e16cca89b4f
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-24 13:19:46 +0800

    add efficientnet

--- custom/models/torchvisionmodels.py
+++ custom/models/torchvisionmodels.py
@@ -3,6 +3,23 @@ import torch.nn as nn
 from manafaln.core.builders import ModelBuilder
 from torchvision.models import efficientnet_v2_s
 
+
+class EfficientNet(nn.Module):
+    def __init__(
+        self, 
+        model_config: dict,
+        num_classes: int,
+    ):
+        super().__init__()
+        self.backbone: torch.nn.Module = ModelBuilder()(model_config)
+        self.linear = nn.Linear(1000, num_classes)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.linear(x)
+        return x
+    
+
 class EfficientNetV2(nn.Module):
     def __init__(
         self, 
