8334170b4b79fde827a438136f74e552aa13a2f5
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-24 21:20:23 +0800

    update ensemble scripts

--- custom/models/efficientnet_v2_ensemble.py
+++ custom/models/efficientnet_v2_ensemble.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 from manafaln.core.builders import ModelBuilder
-from torchvision.models import efficientnet_v2_s, efficientnet_v2_m
+from torchvision.models import efficientnet_v2_s, efficientnet_v2_m, efficientnet_v2_l
 
 #%%
 
@@ -14,12 +14,14 @@ class efficientnetv2_ensemble(nn.Module):
         super().__init__()
         self.model1 = efficientnet_v2_s(weights='DEFAULT')
         self.model2 = efficientnet_v2_m(weights='DEFAULT')
+        self.model3 = efficientnet_v2_l(weights='DEFAULT')
         self.linear = nn.Linear(1000, num_classes)
 
     def forward(self, x):
         x1 = self.model1(x)
         x2 = self.model2(x)
-        x_combined = (x1 + x2)/2
+        x3 = self.model3(x)
+        x_combined = (x1 + x2 + x3)/3
         x_output = self.linear(x_combined)
         return x_output
 
