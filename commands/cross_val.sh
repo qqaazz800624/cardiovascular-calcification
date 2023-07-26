4bb8367dd8799da15b4651d342faa366eb404e00
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-26 23:18:41 +0800

    update shell scripts

--- commands/cross_val.sh
+++ commands/cross_val.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m manafaln.utils.cross_validation \
-    -c ${2:-"configs/2_cac_classification/train_effiV2ensemble.yaml"}\
+    -c ${2:-"configs/2_cac_classification/train_b2.yaml"}\
     -k ${3:-10}\
     -e \
     -v ${1:-1} 
