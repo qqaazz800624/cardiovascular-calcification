9d9aa45e740626de1a543adc5457a6ccc09ef949
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-27 16:46:05 +0800

    update shell scripts

--- commands/test.sh
+++ commands/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python -m manafaln.apps.validate \
-    -c ${2:-"configs/2_cac_classification/test_b.yaml"} \
-    -f lightning_logs/version_2/fold_9/checkpoints/best_model.ckpt
+    -c ${1:-"configs/2_cac_classification/test_b.yaml"} \ 
+    -f lightning_logs/version_4/fold_0/checkpoints/best_model.ckpt
     #-f lightning_logs/$1/checkpoints/best_model.ckpt
