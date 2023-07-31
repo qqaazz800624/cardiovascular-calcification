36b0e899717348e4debef17feeeac1a8bf12d467
qqaazz800624 <qqaazz800624@gmail.com>
2023-07-31 16:07:04 +0800

    update shell scripts

--- commands/cross_val.sh
+++ commands/cross_val.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python -m manafaln.utils.cross_validation \
-    -c ${2:-"configs/2_cac_classification/train_b2.yaml"}\
+    -c ${2:-"configs/2_cac_classification/train_b.yaml"}\
     -k ${3:-10}\
     -e \
-    -v ${1:-1} 
+    -v ${1:-6} 
 
--- commands/test_seg.sh
+++ commands/test_seg.sh
@@ -1,4 +1,4 @@
 python -m manafaln.apps.validate \
-    -c ${1:-"configs/1_heart_segmentation/train_b.yaml"} \ 
-    -f lightning_logs/version_4/fold_0/checkpoints/best_model.ckpt
+    -c ${2:-"configs/1_heart_segmentation/train_b.yaml"} \
+    -f lightning_logs/version_5/fold_$1/checkpoints/best_model.ckpt
     #-f lightning_logs/$1/checkpoints/best_model.ckpt
