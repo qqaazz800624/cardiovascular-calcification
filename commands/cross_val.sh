6014b8218489c3d32e6a0d91dae6148d14eb3470
qqaazz800624 <qqaazz800624@gmail.com>
2023-08-10 18:26:36 +0800

    update cv shell script

--- commands/cross_val.sh
+++ commands/cross_val.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m manafaln.utils.cross_validation \
-    -c ${2:-"configs/2_cac_classification/train_b.yaml"}\
+    -c ${2:-"configs/2_cac_classification/train_efficientnetv2.yaml"}\
     -k ${3:-10}\
     -e \
     -v ${1:-1} 
