9930776bba3155bee4d3dd826fad644c6492a9ce
qqaazz800624 <qqaazz800624@gmail.com>
2023-10-02 23:24:14 +0800

    update shell scripts

--- commands/predict.sh
+++ commands/predict.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python -m manafaln.apps.predict \
-    -c ${1:-"configs/1_heart_segmentation/predict.yaml"} \
-    -f custom/transforms/heart_seg.ckpt
+    -c ${1:-"configs/1_heart_segmentation/predict_b.yaml"} \
+    -f custom/transforms/heart_seg_dropout.ckpt
     #-f lightning_logs/$1/checkpoints/best_model.ckpt
--- commands/train.sh
+++ commands/train.sh
@@ -2,4 +2,4 @@
 
 python -m manafaln.apps.train \
     -s 42 \
-    -c ${1:-"configs/2_cac_classification/train_b.yaml"}
+    -c ${1:-"configs/1_heart_segmentation/train_b_dropout.yaml"}
