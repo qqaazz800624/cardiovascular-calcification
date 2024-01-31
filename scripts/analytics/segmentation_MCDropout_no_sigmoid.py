c86a3961444269d94ac2b8d2f6af29bada8ecebd
qqaazz800624 <qqaazz800624@gmail.com>
2024-01-31 15:52:51 +0800

    rename modules

--- scripts/analytics/segmentation_MCDropout_no_sigmoid.py
+++ scripts/analytics/segmentation_MCDropout_no_sigmoid.py
@@ -1,18 +1,14 @@
 import torch
-from monai.transforms import Transform, MaskIntensity, AsDiscrete
-
 from manafaln.core.builders import ModelBuilder
 
 device = "cuda:3" if torch.cuda.is_available() else "cuda:2"
 
-class SegmentationMCDropout_alea():
+class SegmentationMCDropout():
     def __init__(
         self, model_config: dict, model_weight: str, num_samples: int = 10
     ):
         self.num_samples = num_samples
-
         self.model: torch.nn.Module = ModelBuilder()(model_config)
-
         model_weight = torch.load(model_weight, map_location="cpu")["state_dict"]
         for k in list(model_weight.keys()):
             k_new = k.replace(
@@ -22,18 +18,17 @@ class SegmentationMCDropout_alea():
 
         self.model.load_state_dict(model_weight)
         self.model.to(device)
-        #self.model.eval()
+        
 
     def __call__(self, data):
         # img shape (C, W, H) => (B, C, W, H)
         img = data.unsqueeze(0)
-        #discreter = AsDiscrete(threshold=0.5)
         sample_masks = []
         self.model.eval()
         self.model.enable_random(self.model)
         for _ in range(self.num_samples):
             logit = self.model.random_forward(img)
-            mask_heart = torch.sigmoid(logit)[0, 2]    # take segmentation mask of heart
+            mask_heart = logit[0, 2]  # take segmentation mask of heart and use original logits without sigmoid functions
             mask_heart = mask_heart.detach().cpu()
             sample_masks.append(mask_heart)
 
